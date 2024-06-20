db862a40e24511c1984d5d009b1a0d4a5915ea13
Fábio de Moraes Ribeiro <fmribeiro.info@gmail.com>
2024-06-19 23:43:10 -0300

    Utilizando  get, click, contain, type e should
    Validação de campos durante 7 exercícios práticos

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,11 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', () => {
+    cy.get('#firstName')
+      .type('Fábio de Moraes', {delay:0})
+    cy.get('#lastName')
+      .type('Ribeiro', {delay:0})
+    cy.get('#email')
+      .type('mail@mail.com', {delay:0})
+    cy.get('#open-text-area')
+      .type('Quero ser milionário', {delay:0})
+    cy.contains('.button', 'Enviar').click();
+})
